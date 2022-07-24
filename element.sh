7db630760545508a25167c97cb62f20f1270a7c8
SergioMate <sergiomatecastro@gmail.com>
2022-07-24 21:29:04 +0000

    feat: search element

--- element.sh
+++ element.sh
@@ -6,7 +6,13 @@ if [ -z $1 ]
 then
   echo "Please provide an element as an argument."
 else
-  ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")
-  SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE symbol=$1")
-  NAME=$($PSQL "SELECT name FROM elements WHERE name=$1")
+  if [[ "$1" =~ ^[0-9]+$ ]]
+  then
+    CONDITION="atomic_number=$1"
+  else
+    CONDITION="symbol='$1' OR name='$1'"
+  fi
+
+  SELECTED=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, name, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE $CONDITION")
+  echo $SELECTED
 fi
